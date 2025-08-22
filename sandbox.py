e9413a72dd0536e33b271e304491ced002f3fb69
Joshua Monteiro <joshua.monteiro@halter.co.nz>
2025-08-22 09:12:17 +0700

    add sandbox file to mess around with

--- sandbox.py
+++ sandbox.py
@@ -1,19 +1,24 @@
 from EasyMCP2221 import MCP2221
 
 if __name__ == '__main__':
-    usb_serials = ['0004688946',
-                   '0006179578',
-                   '0006178676',
-                   '0006176088',
-                   '0004694576',
-                   '0004699196']
+    usb_serials = [
+                "0006180251",
+                "0006178898",
+                "0006176117",
+                "0006177280",
+                "0006179096",
+                "0006177783",
+                   ]
     for usb_serial in usb_serials:
         try:
             mcp = MCP2221.Device(usbserial=usb_serial)
             print(f"Connected to MCP2221 with USB serial: {usb_serial}")
             mcp.set_flash_protection(MCP2221.WriteProtection.PROTECTED, b'\x00\x00\x00\x00\x00\x00\x00\x00')
-            print(mcp.get_all_gp_settings())
-
+            # print(mcp.get_all_gp_settings())
             print(mcp.get_all_chip_settings())
         except Exception as e:
             print(f"Failed to connect to MCP2221 with USB serial {usb_serial}: {e}")
+
+    # mcp = MCP2221.Device(usbserial="0006176088")
+    # entry = {"gp0": "GPIO_OUT", "gp1": "GPIO_IN"}
+    # mcp.set_pin_function(**entry)
