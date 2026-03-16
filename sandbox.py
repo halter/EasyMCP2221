2b9762f4c729a5940e90d6c78a1fc639df5d1bb3
Joshua Monteiro <joshua.monteiro@halter.co.nz>
2026-03-17 10:51:27 +1300

    add comment for sandbox use

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,8 @@
 from EasyMCP2221 import MCP2221
 
 if __name__ == '__main__':
+    # NOTE: quick and dirty script to set the write protection on the MCP2221 devices (intended for ICT use). Make sure that you first set GPIO settings before applying
+    # the write-protection.
     usb_serials = [
                 "0006180251",
                 "0006178898",
@@ -13,12 +15,11 @@ if __name__ == '__main__':
         try:
             mcp = MCP2221.Device(usbserial=usb_serial)
             print(f"Connected to MCP2221 with USB serial: {usb_serial}")
-            mcp.set_flash_protection(MCP2221.WriteProtection.PROTECTED, b'\x00\x00\x00\x00\x00\x00\x00\x00')
-            # print(mcp.get_all_gp_settings())
+            print(mcp.get_all_gp_settings())
             print(mcp.get_all_chip_settings())
+            usr = input('Apply write protection? (y/n): ')
+            if usr.lower() == 'y':
+                mcp.set_flash_protection(MCP2221.WriteProtection.PROTECTED, b'\x00\x00\x00\x00\x00\x00\x00\x00')
+
         except Exception as e:
             print(f"Failed to connect to MCP2221 with USB serial {usb_serial}: {e}")
-
-    # mcp = MCP2221.Device(usbserial="0006176088")
-    # entry = {"gp0": "GPIO_OUT", "gp1": "GPIO_IN"}
-    # mcp.set_pin_function(**entry)
